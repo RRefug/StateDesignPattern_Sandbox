74ddc03101035a6e2cea778ce5d747c2145c0ec0
RRefug <99227044+RRefug@users.noreply.github.com>
2025-09-22 19:02:21 -0700

    Implemented the state design pattern. Officially done learning it. Ready to move forward fearlessly!

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 Intrigued with clean code, refactoring, system design, and code architecture. I remembered design patterns and was hoping to use some in an ongoing project. Eager to deepen my understanding, I started exploring the **State Design Pattern** through a small work-in-progress project.
 
-I used online resources to guide my initial outline and built the project based on pseudocode examples. Along the way, I improved my ability to read UML class diagrams and interpret relationships like aggregation and association — skills that will definitely help me implement other patterns in future projects, including my ongoing **Alien Invasion** refactor.
+I used online resources to guide my initial outline and built the project based on pseudocode examples. Along the way, I improved my ability to read UML class diagrams and interpret relationships like aggregation and association — skills that will definitely help me implement other patterns in future projects, including my ongoing **Alien Invasion** refactor. 
 
 <img width="773" height="550" alt="stateDesignPattern" src="https://github.com/user-attachments/assets/f483e9a6-d9df-4e9a-8a4e-a206b025c904" />
 
@@ -16,13 +16,22 @@ I used online resources to guide my initial outline and built the project based 
 
 
 
-This repo is a sandbox for experimenting with architectural ideas. Right now, I'm tackling a circular import issue and working toward a cleaner package structure using `__init__.py` files to expose interfaces.
+This repo is a sandbox for experimenting with architectural ideas with the State Design Pattern. Right now, I'm tackling a circular import issue and working toward a cleaner package structure using `__init__.py` files to expose interfaces. I got this state design pattern working! Not the way I intended to originally. I've learned that there will be times when it's good to keep certain classes together in one module rather than many when they rely strongly on one another. 
 
-Stay tuned—this project is evolving as I learn.
+" The only time I think it's correct to use more than one class per file is when you are using internal classes... but internal classes are inside another class, and thus can be left inside the same file. The inner classes roles are strongly related to the outer classes, so placing them in the same file is fine."
+     - Quote from https://stackoverflow.com/questions/360643/is-it-a-bad-practice-to-have-multiple-classes-in-the-same-file
+
+I tried to separate one module because it had more than one class within it. However, it led to my issue with circular imports for a couple days. Circular imports happen when two, or more, different modules try to import the classes stored within themselves before the entire module can finish initializing (Ex: ClassA tries to import ClassB while ClassB is importing ClassA).
+
+Circular imports also means 'smelly code' and despite me following multiple pseudocodes from the internet, it always led to a circular import issue. Preventing me from even getting past the first step. To my discovery, Python worries about circular imports more than other programming languages. However, I found a pseudocode that works and I was able to trace the code, see what was underneathe the hood, and follow it from start to finish. If you want to run it for yourself, visit the site Refacturing Guru and look at the python example. Link: https://refactoring.guru/design-patterns/state/python/example#lang-features
+Or see my detailed explanation within state_pkg. Run the main.py module and bam! Works.
+
+Besides that, I have also remembered, but totally forgot, that it is 100% okay to know when you should try aa different approach. That is what happened with main_pkg. That was the original pseudocode I tried following, but then led to me following another pseudocode, both suffering from the circular import issue. However, a glimmer of hope finally hit and I found a working example. Learned a lot from it and about python. This sandbox became a smaller project on its own. HOLY COW!!! Feels good that I didn't give up. I'm making the time to learn and I am very proud of this small step forward with A LOT of knowledge under my belt. 
+
+Now that I am finished with this state design pattern, I have learned a lot more than I thought I would and ready to carry on with my bigger project, Alien Invasion, but refactored into a more efficient and tested version. This is definitely going to make other design patterns and cleaner code a breeze. 
 
 
-
-
+What I got out of this: 
 - Understand and implement the State Design Pattern
 - Practice reading and applying UML diagrams
 - Explore package structure and import management in Python
--- state_pkg/main.py
+++ state_pkg/main.py
@@ -1,49 +1,55 @@
-from __future__ import annotations
+from __future__ import annotations # allows us to avoid namespace errors since some classes need to be defined before others using them. Ex: AudioPlayer needs to use State interface, but it hasnt't been defined yet. 
 from abc import ABC, abstractmethod
 
-class Context:
+class AudioPlayer:
     """
     The Context defines the interface of interest to clients. It also maintains
     a reference to an instance of a State subclass, which represents the current
     state of the Context.
     """
-    _state = None # A reference to the current state of the Context.
+    _currentState = None # A reference to the current state of the Context. Protected variable
 
-    def __init__(self, state: State) -> None:
+    def __init__(self, state: State) -> None: # returns nothing. 
         self.transition_to(state)
 
     def transition_to(self, state: State):
         '''The context allows changing the state object at runtime.'''
 
-        print(f"Context: Transition to {type(state).__name__}")
-        self._state = state
-        self._state.context = self
+        print(f"***** Context: Transition to {type(state).__name__}") # checks the type of the state object by grabbing name of it's class.
+        self._currentState = state # Ready state object is set as current state
+        self._currentState.context = self # the current audioplayer object is then set to the attribute given by the interface class. 
 
-        """
-        The Context delegates part of its behavior to the current State object.
-        """
-
-    def request1(self):
-        self._state.handle1()
+    """
+    The Context delegates(passes) part of its behavior to the current State object.
+    """
+    # context methods that call interface methods belonging to concrete state objects. 
+    # Works because _currentState houses a Ready or Playing state object.  
+    def request1(self): # Which both types of state objects inherit from the interface.
+        self._currentState.handle1()
 
     def request2(self):
-        self._state.handle2()
+        self._currentState.handle2()
+
 
 
 class State(ABC):
     """
     The base State class declares methods that all Concrete State should
     implement and also provides a backreference to the Context object,
-    associated with the State. This backreference can be used by States to
-    transition the Context to another State.
+    associated with the State. 
+    This backreference can be used by States to transition the Context to another State.
     """
 
-    @property
-    def context(self) -> Context:
+    @property 
+    def context(self) -> AudioPlayer:
+        '''
+        Returns an audioplayer object as an attribute so that different State objects may have access for using the context(AudioPlayer) methods. 
+        The method, that allows us to transition the context to another state, transition_to(different State).
+        '''
         return self._context
     
     @context.setter
-    def context(self, context: Context) -> None:
+    def context(self, context: AudioPlayer) -> None:
         self._context = context
     
     @abstractmethod
@@ -60,41 +66,51 @@ Context.
 """
 
 
-class ConcreteStateA(State):
+class Ready(State):
+    '''Ready state ( or ConcreteStateA) implements abstract methods with the behavior we wish to see in a ready state for an audioplayer. '''
     def handle1(self) -> None:
-        print('ConcreteStateA handles rquest1.')
+        print('ConcreteStateA handles request1.')
         print('ConcreteStateA wants to change the state of the context.')
-        self.context.transition_to(ConcreteStateB())
+
+        # Current Ready object uses the context attribute it inherited from State to call method from context class. 
+        # Passes a Playing state object as an argument. 
+        self.context.transition_to(Playing())
 
     def handle2(self) -> None:
-        print('ConcreteStateA handles request2. ')
+        print('ConcreteStateA handles request2.')
     
 
-class ConcreteStateB(State):
+class Playing(State):
+    '''Playing state ( or ConcreteStateB) implements abstract methods with the behavior we wish to see in a playing state for an audioplayer. '''
     def handle1(self) -> None:
-        print('ConcreteStateA handles request2.')
+        print('ConcreteStateB handles request1.')
 
     def handle2(self)-> None:
-        print("ConcreteStateB handles request2.")
-        print("ConcreteStateB wants to change the state of the context.")
-        self.context.transition_to(ConcreteStateA())
+        print('ConcreteStateB handles request2.')
+        print('ConcreteStateB wants to change the state of the context.')
+        self.context.transition_to(Ready())
 
 
 
 if __name__ == "__main__":
-    # The client code.
+    
+    context = AudioPlayer(Ready()) # Passing in concrete state object as argument. After this line, Context class starts with the ready state instance. 
+    context.request1() # After this function call, context is now in the Playing state. 
+    context.request2() 
 
-    context = Context(ConcreteStateA())
-    context.request1()
-    context.request2()
 
     # TODO: Works, but I want to separate the module into separate packages. 
 
+
+
     # OUTPUT:
-    # Context: Transition to ConcreteStateA
+    # ***** Context: Transition to ConcreteStateA
     # ConcreteStateA handles request1.
     # ConcreteStateA wants to change the state of the context.
-    # Context: Transition to ConcreteStateB
+    # ***** Context: Transition to ConcreteStateB
     # ConcreteStateB handles request2.
     # ConcreteStateB wants to change the state of the context.
-    # Context: Transition to ConcreteStateA
+    # ***** Context: Transition to ConcreteStateA
+
+
+
